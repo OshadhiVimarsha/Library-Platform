7de45ead80c747b88f8a62524750a5538725ed5e
oshivimarsha <oshadivimarsha@gmail.com>
2026-03-31 15:20:47 +0100

    Update yaml file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   apps : [
     {
-      name: "Cloud-sql-auth-proxy",
-      script: "./cloud-sql-proxy project-b2a5e362-17b9-4abe-95d:asia-southeast1:mysql-vm project-b2a5e362-17b9-4abe-95d:asia-southeast1:postgres-vm --private-ip",
-      log_file: "./logs/cloud-sql-proxy.log",
-    },
-    {
       name   : "config-server",
       script : "java",
       args   : "-jar ./config-server/target/config-server-1.0.0.jar",
